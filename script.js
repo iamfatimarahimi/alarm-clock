052769adf07d7a65db335702e9383e4a42044255
fatima rahimi <iamfatimarahimi@gmail.com>
2023-10-22 21:52:15 +0330

    selecting button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const selectMenu = document.querySelectorAll('select')
 const timeBox = document.querySelector('.time')
+const setAlarmBtn = document.querySelector('button')
 for(let i = 23 ; i >=0 ; i--){
     i = i<10? '0'+i : i;
     // if(i<10){
@@ -15,17 +16,6 @@ for(let i = 59 ; i >=0 ; i--){
     let option = `<option value="${i}">${i}</option>`;
     selectMenu[1].firstElementChild.insertAdjacentHTML('afterend' , option)
 }   
-setInterval(()=>{
-    let date =new Date();
-    let hour =date.getHours();
-    let minuet =date.getMinutes();
-    let second =date.getSeconds();
-
-    hour = hour<10? '0'+ hour: hour;
-    minuet = minuet<10? '0'+ minuet: minuet;
-    second = second<10? '0'+ second: second;
-    timeBox.innerHTML = `${hour}: ${minuet}: ${second}`;
-},1000)
 setInterval(()=>{
     let date =new Date();
     let hour =date.getHours();
